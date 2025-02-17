fde0727841a593902dc152fc8a54d63c544ce8e5
practicas.progra@conectaconsultores.es <practicas.progra@conectaconsultores.es>
2025-02-17 16:58:39 +0100

    creada la clase basculaIS30

--- data_ArrivalORIGINVBN.cs
+++ data_ArrivalORIGINVBN.cs
@@ -1,16 +1,13 @@
-ConfigurationManager.AppSettings["nombreEnAPPCONFIG"]
+'DATA ARRIVAL DE WINSOCK
 
 
-//DATA ARRIVAL DE WINSOCK
-
-
-Private Sub Winsock1_DataArrival(ByVal bytesTotal As Long)
+Private Sub myBasculaIS30_DataArrival(ByVal bytesTotal As Long)
 
 
 	
-    Dim NumCar As Long  // LONGITUD DE LA INFORMACION QUE NECESITAMOS 
-    Dim Info As String  //LA INFORMACION QUE NECESITAMOS 
-    Dim Peso As String  // NO SE UTILIZA EN IS 30
+    Dim NumCar As Long  ' LONGITUD DE LA INFORMACION QUE NECESITAMOS 
+    Dim Info As String  'LA INFORMACION QUE NECESITAMOS 
+    Dim Peso As String  ' NO SE UTILIZA EN IS 30
     Dim pesoEstable As Boolean
     
    
@@ -18,21 +15,21 @@ Private Sub Winsock1_DataArrival(ByVal bytesTotal As Long)
  
     
 	NumCar = bytesTotal
-	Winsock1.GetData Info , vbString, NumCar // INFO es la variable, VBSTRING es el tipo y NUMCAR es la longitud de la recogida de datos a guardar en INFO 
+	Winsock1.GetData Info , vbString, NumCar ' INFO es la variable, VBSTRING es el tipo y NUMCAR es la longitud de la recogida de datos a guardar en INFO 
 
-	If Info <> Chr$(ACK) Then    //ACK ES LA SEÑAL DE RECONOCIMIENTO STANDARD, POR ESO EL SER DISTINTO DE ACK IMPLICA QUE HEMOS RECIBIDO INFORMACION VALIDA (INFO) Y POR ENDE PODEMOS PROCESARLA
+	If Info <> Chr$(ACK) Then    'ACK ES LA SEÑAL DE RECONOCIMIENTO STANDARD, POR ESO EL SER DISTINTO DE ACK IMPLICA QUE HEMOS RECIBIDO INFORMACION VALIDA (INFO) Y POR ENDE PODEMOS PROCESARLA
 
-            //Envia datos ACK
-		Winsock1.SendData Chr$(ACK)  //CONFIRMA LA RECEPCION DE LOS DATOS AL SERVIDOR 
+            'Envia datos ACK
+		Winsock1.SendData Chr$(ACK)  'CONFIRMA LA RECEPCION DE LOS DATOS AL SERVIDOR 
             
-		pesoEstable = RecPeso_IS30_RED(Info) //Esta función procesa los datos recibidos (Datos) y determina si el peso recibido es estable. LOS GUARDA EN GBLPESO
+		pesoEstable = RecPeso_IS30_RED(Info) 'Esta función procesa los datos recibidos (Datos) y determina si el peso recibido es estable. LOS GUARDA EN GBLPESO
     
-		pesoEstable = PintaPesoST_Estable()
-		
+		pesoEstable = PintaPesoST_Estable()   'Esta función muestra el peso estable y realiza algunas acciones adicionales basadas en el estado de objPartida vuelve a guardar en GBLPESO .
+
 		CalculaEstadoCanalEstable True
         
-            //TrataEstadoCanal
-            //objMensajeLog.GuardaMensajeGeneral Now() & " Winsock1_DataArrival " & Info & vbCrLf & Peso & vbCrLf & gblPeso
+            'TrataEstadoCanal
+            'objMensajeLog.GuardaMensajeGeneral Now() & " Winsock1_DataArrival " & Info & vbCrLf & Peso & vbCrLf & gblPeso
             
 	End If
     
@@ -40,6 +37,141 @@ Private Sub Winsock1_DataArrival(ByVal bytesTotal As Long)
 
 End Sub
 
+'RecPeso_IS30_RED 'Esta función procesa los datos recibidos (Datos) y determina si el peso recibido es estable. LOS GUARDA EN GBLPESO
+
+Private Function RecPeso_IS30_RED(Datos As String) As Boolean
+    Dim PesSt As String
+    Dim pesoEstable As Boolean
+    
+    Dim status As String
+    Dim iStatus As Integer
+    Dim iMod As Integer
+    
+    Dim signo As String
+    Dim sinTara As String
+    
+    'Const SOH = Chr(1)
+    'Const STX = Chr(2)
+    'Const ETX = Chr(3)
+    'Const ENQ = Chr(5)
+    'Const ACK = Chr(6)
+    'Const LF = Chr(10)
+    'Const CR = Chr(13)
+    'Const NACK = Chr(21)
+    'Const GS = Chr(29)
+    
+    
+    Dim patron As String
+    'patron = "^" + Chr(1) & "0" & Chr(3) & "1" & Chr(3) & "254" & Chr(3) & "I!GV01\S*\|GD01\|(\S*)\|\S*GD02\S*LX02" & Chr(13) + Chr(10) + "$"
+    patron = "^" + Chr(1) & "0" & Chr(3) & "1" & Chr(3) & "254" & Chr(3) & "I!RV04\S*\|GD01\|(\S*)\|\S*GD02\S*LX02" & Chr(13) + Chr(10) + "$"
+    ' patron = "^" + "I!GV01\S*\|GD01\|(\S*)\|\S*GD02\S*LX02" + "$"
+
+    Dim rexpEntrada As RegExp
+    'Dim myMatches As MatchCollection
+    'Dim myMatch As Match
+    
+    Set rexpEntrada = New RegExp
+    rexpEntrada.Pattern = patron
+
+    If rexpEntrada.Test(Datos) Then
+    
+        pesoEstable = True
+        
+        Dim strPeso As String
+        strPeso = (rexpEntrada.Replace(Datos, "$1"))
+        
+        Dim wordsPeso() As String
+        wordsPeso = Split(strPeso, ";")
+        
+        Dim nDecimales As Integer
+        Dim iPeso As Integer
+        Dim dPeso As Double
+        Dim i As Integer
+        For i = 0 To UBound(wordsPeso)
+        
+            If i = 0 Then
+            
+            ElseIf i = 1 Then
+                nDecimales = wordsPeso(i)
+                nDecimales = Abs(nDecimales)
+            ElseIf i = 2 Then
+                iPeso = wordsPeso(i)
+            End If
+                        
+        Next
+        
+        dPeso = iPeso / (10 ^ nDecimales)
 
 
+        gblPeso = dPeso   'Aqui guarda el peso
 
+
+        NetoLbl.Visible = True
+        
+       
+    Else
+        pesoEstable = False
+    End If
+    
+    RecPeso_IS30_RED = pesoEstable
+    
+End Function
+
+
+'PintaPesosST_Estable
+
+Private Function PintaPesoST_Estable() As Boolean 'Esta función muestra el peso estable y realiza algunas acciones adicionales basadas en el estado de objPartida.
+
+    Dim mpesoEstable As Boolean
+    Dim PesoST As Double
+
+    
+	mpesoEstable = True
+	PesoST = gblPeso
+        
+   
+
+    
+    gblPeso = CDbl(PesoST) 'Incluido para obtener el peso como una variable global
+    If gblPeso > 10 And Not objPartida Is Nothing Then
+    
+        gblPeso = gblPeso - objPartida.Tara
+        pesoDefinitivoDeVerdad = gblPeso
+        
+        If objPartida.EnEdicion Then
+                                
+           ' Vuelve a permitir la edici?n
+            AvanceBtn.Enabled = True
+            RetrocedeBtn.Enabled = True
+            
+            objUF.ReInit
+            objRechazo.ReInit
+            objDecomiso.ReInit
+                       
+            If objPartida.esDelConsejo Then
+                gblTipoSexo = TipoSexo.Macho
+            Else
+                gblTipoSexo = TipoSexo.SC
+            End If
+            
+            'Al Entran en b?scula se sale de edici?n
+            objPartida.EnEdicion = False
+            BtnCambiar.Visible = False
+            btnBorrarCanal.Visible = False
+                    
+            objPartida.AvanceCanalFinal
+                    
+            PantallaInicial
+            
+        End If
+        
+        WeightLbl.Caption = Format(pesoDefinitivoDeVerdad, "##0.000")
+        KGLbl.ForeColor = ColorVerde
+        
+    End If
+    
+    ' NetoLbl.Visible = (tara <> 0)
+
+    PintaPesoST_Estable = mpesoEstable 'Incluido para saber si el peso es estable
+    
+End Function
---
+++ myBasculaIS30.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class MyBasculaIS30
+{
+    private Socket _socket;
+    private string _ipAddress;
+    private int _port;
+
+    // Constructor
+    public MyBasculaIS30(string ipAddress, string port)
+    {
+        _ipAddress = ipAddress;
+        _port = int.Parse(port);
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    }
+
+    // Método para iniciar el servidor
+    public void StartServer()
+    {
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(_ipAddress), _port);
+        _socket.Bind(localEndPoint);
+        _socket.Listen(10);
+
+        Console.WriteLine($"Servidor iniciado en {_ipAddress}:{_port}. Esperando conexiones...");
+
+        Socket clientSocket = _socket.Accept();
+        Console.WriteLine("Conexión aceptada.");
+
+        // Leer datos del cliente
+        byte[] buffer = new byte[1024];
+        int bytesRead = clientSocket.Receive(buffer);
+        Console.WriteLine($"Datos recibidos: {Encoding.UTF8.GetString(buffer, 0, bytesRead)}");
+
+        // Cerrar el socket
+        clientSocket.Close();
+        _socket.Close();
+    }
+
+    // Método para conectarse a un servidor
+    public void Connect()
+    {
+        _socket.Connect(new IPEndPoint(IPAddress.Parse(_ipAddress), _port));
+        Console.WriteLine($"Conectado a {_ipAddress}:{_port}");
+
+        // Enviar datos de ejemplo al servidor
+        string message = "Hola desde el cliente!";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        _socket.Send(data);
+
+        // Cerrar el socket
+        _socket.Close();
+    }
+}
+
+
+private void MyBasculaIS30_DataArrival(long bytesTotal)
+{
+    long NumCar = bytesTotal;  // LONGITUD DE LA INFORMACION QUE NECESITAMOS
+    string Info = string.Empty;  // LA INFORMACION QUE NECESITAMOS
+    string Peso = string.Empty;  // NO SE UTILIZA EN IS 30
+    bool pesoEstable;
+
+    // Obtener los datos recibidos
+    byte[] buffer = new byte[NumCar];
+    Winsock1.Receive(buffer);
+    Info = Encoding.UTF8.GetString(buffer);
+
+    // ACK ES LA SEÑAL DE RECONOCIMIENTO STANDARD, POR ESO EL SER DISTINTO DE ACK IMPLICA QUE HEMOS RECIBIDO INFORMACION VALIDA (INFO) Y POR ENDE PODEMOS PROCESARLA
+    if (Info != ((char)6).ToString())
+    {
+        // Envia datos ACK
+        byte[] ack = Encoding.UTF8.GetBytes(((char)6).ToString());
+        Winsock1.Send(ack);  // CONFIRMA LA RECEPCION DE LOS DATOS AL SERVIDOR
+
+        pesoEstable = RecPeso_IS30_RED(Info);  // Esta función procesa los datos recibidos y determina si el peso recibido es estable
+        pesoEstable = PintaPesoST_Estable();   // Esta función muestra el peso estable y realiza algunas acciones adicionales basadas en el estado de objPartida
+
+        CalculaEstadoCanalEstable(true);
+    }
+}
+
+
+public bool RecPeso_IS30_RED(string datos)   // Esta función procesa los datos recibidos y determina si el peso recibido es estable
+{
+    string PesSt;
+    bool pesoEstable;
+
+    string status;
+    int iStatus;
+    int iMod;
+
+    string signo;
+    string sinTara;
+
+    string patron = "^" + ((char)1) + "0" + ((char)3) + "1" + ((char)3) + "254" + ((char)3) + "I!RV04\\S*\\|GD01\\|(\\S*)\\|\\S*GD02\\S*LX02" + ((char)13) + ((char)10) + "$";
+
+    System.Text.RegularExpressions.Regex rexpEntrada = new System.Text.RegularExpressions.Regex(patron);
+
+    if (rexpEntrada.IsMatch(datos))
+    {
+        pesoEstable = true;
+        
+        string strPeso = rexpEntrada.Replace(datos, "$1");
+        string[] wordsPeso = strPeso.Split(';');
+
+        int nDecimales = 0;
+        int iPeso = 0;
+        double dPeso = 0.0;
+
+        for (int i = 0; i <= wordsPeso.Length - 1; i++)
+        {
+            if (i == 0)
+            {
+                // No se hace nada
+            }
+            else if (i == 1)
+            {
+                nDecimales = int.Parse(wordsPeso[i]);
+                nDecimales = Math.Abs(nDecimales);
+            }
+            else if (i == 2)
+            {
+                iPeso = int.Parse(wordsPeso[i]);
+            }
+        }
+
+        dPeso = iPeso / Math.Pow(10, nDecimales);
+        gblPeso = dPeso;   // Aqui guarda el peso
+
+        NetoLbl.Visible = true;
+    }
+    else
+    {
+        pesoEstable = false;
+    }
+
+    return pesoEstable;
+}
+
+public bool PintaPesoST_Estable()  // Esta función muestra el peso estable y realiza algunas acciones adicionales basadas en el estado de objPartida
+{
+    bool mpesoEstable;
+    double PesoST;
+
+    mpesoEstable = true;
+    PesoST = gblPeso;
+
+    gblPeso = Convert.ToDouble(PesoST); // Incluido para obtener el peso como una variable global
+    if (gblPeso > 10 && objPartida != null)
+    {
+        gblPeso -= objPartida.Tara;
+        pesoDefinitivoDeVerdad = gblPeso;
+
+        if (objPartida.EnEdicion)
+        {
+            // Vuelve a permitir la edición
+            AvanceBtn.Enabled = true;
+            RetrocedeBtn.Enabled = true;
+
+            objUF.ReInit();
+            objRechazo.ReInit();
+            objDecomiso.ReInit();
+
+            if (objPartida.EsDelConsejo)
+            {
+                gblTipoSexo = TipoSexo.Macho;
+            }
+            else
+            {
+                gblTipoSexo = TipoSexo.SC;
+            }
+
+            // Al entrar en báscula se sale de edición
+            objPartida.EnEdicion = false;
+            BtnCambiar.Visible = false;
+            btnBorrarCanal.Visible = false;
+
+            objPartida.AvanceCanalFinal();
+
+            PantallaInicial();
+        }
+
+        WeightLbl.Caption = pesoDefinitivoDeVerdad.ToString("F3");
+        KGLbl.ForeColor = ColorVerde;
+    }
+
+    // NetoLbl.Visible = (tara != 0);
+
+    return mpesoEstable; // Incluido para saber si el peso es estable
+}
+
+public void conectarServer(){
+
+    this.StartServer();
+
+
+}
+
+public void conectarCLient(){
+
+    this.Connect();
+
+
+}
+
+
+
+
