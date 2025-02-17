e25eb22685371efaffe2ce8ed30dee6c84358e0d
practicas.progra@conectaconsultores.es <practicas.progra@conectaconsultores.es>
2025-02-17 12:21:34 +0100

    comentarios de analisis 1

--- data_Arrival.cs
+++ data_Arrival.cs
@@ -8,9 +8,9 @@ Private Sub Winsock1_DataArrival(ByVal bytesTotal As Long)
 
 
 	
-    Dim NumCar As Long
-    Dim Info As String
-    Dim Peso As String
+    Dim NumCar As Long  // LONGITUD DE LA INFORMACION QUE NECESITAMOS 
+    Dim Info As String  //LA INFORMACION QUE NECESITAMOS 
+    Dim Peso As String  // NO SE UTILIZA EN IS 30
     Dim pesoEstable As Boolean
     
    
@@ -18,12 +18,12 @@ Private Sub Winsock1_DataArrival(ByVal bytesTotal As Long)
  
     
 	NumCar = bytesTotal
-	Winsock1.GetData Info, vbString, NumCar
+	Winsock1.GetData Info , vbString, NumCar // INFO es la variable, VBSTRING es el tipo y NUMCAR es la longitud de la recogida de datos a guardar en INFO 
 
-	If Info <> Chr$(ACK) Then
+	If Info <> Chr$(ACK) Then    //ACK ES LA SEÑAL DE RECONOCIMIENTO STANDARD, POR ESO EL SER DISTINTO DE ACK IMPLICA QUE HEMOS RECIBIDO INFORMACION VALIDA (INFO) Y POR ENDE PODEMOS PROCESARLA
 
             //Envia datos ACK
-		Winsock1.SendData Chr$(ACK)
+		Winsock1.SendData Chr$(ACK)  //CONFIRMA LA RECEPCION DE LOS DATOS 
             
 		pesoEstable = RecPeso_IS30_RED(Info)
     
